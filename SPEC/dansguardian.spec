7b78ca77972549690e35ede6428d7a0f88925068
Julien Pivotto <roidelapluie@inuits.eu>
2012-02-21 13:13:27 +0100

    add jenkins build

--- SPEC/dansguardian.spec
+++ SPEC/dansguardian.spec
@@ -7,7 +7,7 @@
 Summary: Content filtering web proxy
 Name: dansguardian
 Version: 2.10.1.1
-Release: 1%{?dist}
+Release: %{jenkins_build}%{?dist}
 License: GPLv2+
 Group: System Environment/Daemons
 URL: http://www.dansguardian.org/
--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ then
         wget "http://dansguardian.org/downloads/2/Stable/dansguardian-2.10.1.1.tar.gz" -O SOURCES/dansguardian-2.10.1.1.tar.gz
     fi
 
-rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" SPEC/dansguardian.spec
+rpmbuild -ba --define="_topdir $PWD" --define="_tmppath $PWD/tmp" --define="jenkins_build $JENKINS_BUILD" SPEC/dansguardian.spec
